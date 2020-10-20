be3d57d4f6de6a8fbe52a07e6013c0a807269257
pengshaohua <75480420@qq.com>
2020-10-20 14:44:22 +0800

    算法题

--- es2018.js
+++ es2018.js
@@ -1,24 +1,27 @@
-let obj = {
-    a: 1,
-    b: 3,
-    c: 5,
-    d: 8
-};
+// let obj = {
+//     a: 1,
+//     b: 3,
+//     c: 5,
+//     d: 8
+// };
 
-let { a, b, ...z } = obj;
+// let { a, b, ...z } = obj;
 
-console.log(1, 2, 3, 4, 5, 6);
+// console.log(1, 2, 3, 4, 5, 6);
 
-console.log(a, b, z);
+// console.log(a, b, z);
 
-let map = [
-    ['name', 'zhangsan'],
-    ['age', '100'],
-];
-console.log(Object.fromEntries(map));
+// let map = [
+//     ['name', 'zhangsan'],
+//     ['age', '100'],
+// ];
+// console.log(Object.fromEntries(map));
+// console.log(123);
 
 
-let str = '    hello world     ';
-console.log(str.trimStart());
-console.log(str.trimEnd());
-console.log('hello');
+// let str = '    hello world     ';
+// console.log(str.trimStart());
+// console.log(str.trimEnd());
+
+
+// console.log([3, 8, 30, 9, 34, 5, 9, 51, 15, ].sort().reverse());
